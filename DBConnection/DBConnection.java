f9df8478a4b31aea7beb10e6128fd1227041dae4
YashikaAggarwal <46573406+YashikaAggarwal@users.noreply.github.com>
2019-12-01 00:35:14 +0530

    Add files via upload

---
+++ DBConnection/DBConnection.java
@@ -0,0 +1,29 @@
+package DBConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection 
+{
+	public static Connection doConnect()
+	{
+		Connection con=null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			 con=DriverManager.getConnection("jdbc:mysql://localhost/carparking","root","");
+			System.out.println("Connected....");
+		} 
+		catch (Exception e) 
+		  {
+			e.printStackTrace();
+		  }
+		return con;
+	}
+	
+	public static void main(String[] args) 
+	{
+		doConnect();
+
+	}
+
+}
